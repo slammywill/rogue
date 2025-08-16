eede9f36445cf7c8bf5385ab06ca1bf13209e256
Sam Willems <swi126@uclive.ac.nz>
2025-08-16 22:10:01 +1200

    Add window title

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,13 @@ use rogue::game::GamePlugin;
 fn main() {
     App::new()
         .add_plugins((
-            DefaultPlugins,
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: String::from("Rogue"),
+                    ..Default::default()
+                }),
+                ..default()
+            }),
             TilemapPlugin,
             GamePlugin,
         ))
