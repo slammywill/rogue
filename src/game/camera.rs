9c40bb75128d61131ca4effddd23e81aa4aeb446
slammywill <samjwillems@protonmail.com>
2025-08-17 00:09:45 +0000

    Add camera zooming with scrollwheel

--- src/game/camera.rs
+++ src/game/camera.rs
@@ -1,10 +1,14 @@
-use bevy::prelude::*;
+use bevy::{
+    input::mouse::{MouseScrollUnit, MouseWheel},
+    prelude::*,
+};
 
 pub struct CameraPlugin;
 
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, initialize_camera);
+        app.add_systems(Update, zoom_control_system);
     }
 }
 
@@ -16,3 +20,25 @@ fn initialize_camera(mut commands: Commands) {
     commands.spawn(MainCamera);
 }
 
+fn zoom_control_system(
+    mut evr_scroll: EventReader<MouseWheel>,
+    mut projection: Single<&mut Projection, With<MainCamera>>,
+) {
+    let Projection::Orthographic(ortho) = projection.as_mut() else {
+        return;
+    };
+
+    for ev in evr_scroll.read() {
+        match ev.unit {
+            MouseScrollUnit::Line => {
+                // Line is for hardware scroll e.g. wheel on mouse
+                ortho.scale += 0.2 * ev.y;
+            }
+            MouseScrollUnit::Pixel => {
+                // Pixel is for smooth scroll e.g. trackpad
+                ortho.scale += 0.2 * ev.y;
+            }
+        }
+    }
+    ortho.scale = ortho.scale.clamp(0.2, 2.0);
+}
